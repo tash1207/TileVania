801968a7869cc0f7a10c69243780955b779fe223
Tasha Hankewych <thankewych@gmail.com>
2025-01-06 13:31:50 -0600

    Set up enemy automatic movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = FindObjectOfType<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb2d.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        moveSpeed = -moveSpeed;
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(moveSpeed), 1f);
+    }
+}
