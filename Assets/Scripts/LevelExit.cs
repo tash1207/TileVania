e67d751a2d164b80087ff70a89cd5d3f65680154
Tasha Hankewych <thankewych@gmail.com>
2025-01-07 14:05:22 -0600

    Add an exit area to each level that loads the next level

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 0.5f;
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+            
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Debug.Log("Load Next");
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
